f11d1139fc30da00cf4059b2d4ce3b1349785526
Hyann <contato.hyann@gmail.com>
2025-05-29 13:11:06 -0300

    Adicionando slideshow a pagina de problemas

--- src/js/script.js
+++ src/js/script.js
@@ -38,6 +38,25 @@ function iniciarMenuSanduiche() {
   }
 }
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // troca a cada 3 segundos
+}
+
+
 // Quiz
 function iniciarQuiz() {
   const quizEl = document.getElementById("quiz");
